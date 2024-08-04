dbb2bfaa6f54f471e76852f96b0965027fa4c4d3
Nageshwar patidar <patidarn7440@gmail.com>
2024-08-05 00:06:30 +0530

    add locationfile

--- my-project/src/pages/LocationMap.tsx
+++ my-project/src/pages/LocationMap.tsx
@@ -1,30 +1,32 @@
-
+import React from 'react';
 import Layout from "./Components/CustomHooks/Layout";
 
-const LocationMap=()=>{
-
-    const divStyle={
+const LocationMap = () => {
+    const divStyle: React.CSSProperties = {
         color: 'blue',
-        textAlign: 'center' ,
-        
-        padding: '20px',     
-        fontFamily: 'Arial',   
+        textAlign: 'center', // Valid value
+        padding: '20px',
+        fontFamily: 'Arial',
         borderRadius: '5px',
-        marginTop: '124px' 
-        
-
+        marginTop: '124px'
     };
-    return(
+
+    return (
         <Layout>
-            <div className="locationMap" style={divStyle}>LocationMap 
+            <div className="locationMap" style={divStyle}>
+                LocationMap
             </div>
             <div className="map">
-                <iframe width="100%" height="600" scrolling="no" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
-
+                <iframe
+                    width="100%"
+                    height="600"
+                    scrolling="no"
+                    src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+                ></iframe>
+            </div>
         </Layout>
     );
-}
-
+};
 
 export default LocationMap;
 
